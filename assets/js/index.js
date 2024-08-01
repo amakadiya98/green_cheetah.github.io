c9d7abba7ee8c3c8dabef4bbdc495c7a936c2c62
amakadiya98 <64594631+amakadiya98@users.noreply.github.com>
2024-08-01 18:11:38 +0530

    login form design done

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     var navbarCollapse = document.getElementById('navbarSupportedContent');
     var headerSmBtn = document.querySelector('.header-sm-btn');
 
@@ -10,3 +10,37 @@ document.addEventListener('DOMContentLoaded', function() {
         headerSmBtn.classList.remove('d-none');
     });
 });
+
+// form
+
+document.addEventListener('DOMContentLoaded', function () {
+    const togglePassword = document.getElementById('togglePassword');
+    const password = document.getElementById('password');
+    const eyeIcon = document.getElementById('eyeIcon');
+
+    togglePassword.addEventListener('click', function () {
+        const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+        password.setAttribute('type', type);
+
+        if (type === 'password') {
+            eyeIcon.src = './assets/images/close-eye.svg';
+        } else {
+            eyeIcon.src = './assets/images/eye-icon.svg';
+        }
+    });
+    
+});
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    const loginButtons = document.querySelectorAll('.login-button-click');
+
+    loginButtons.forEach(button => {
+        button.onclick = function() {
+            window.location.href = 'loginform.html';
+        };
+    });
+});
+
+
+
+
